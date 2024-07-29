7a91a1fd917f532841fac919093ad76b56c9943b
徐國堂 <roberthsu2003@gmail.com>
2024-07-29 07:16:17 +0000

    modify

--- lesson13/lesson13_1.py
+++ lesson13/lesson13_1.py
@@ -23,24 +23,25 @@ def connect_youbike() -> Response | str:
 
 def main():
         response:Response | str = connect_youbike() 
-        if isinstance(response,Response):
-            print("連線成功")
-            data:list[dict] = response.json()    
-            district:str = input("請輸入新北市行政區: ")
-            district += "區"
-            district_stations = []
-            for station in data:
-                if station['sarea'] == district:
-                    district_stations.append(station)
-
-
-            if district_stations:
-                pprint(district_stations)
-            else:
-                print(f"沒有找到 {district} 行政區的站點資訊。請再輸入一次")
-
-        else:
+        if not isinstance(response,Response):
             print(response)
+            return
+        
+        print("連線成功")
+        data:list[dict] = response.json()    
+        district:str = input("請輸入新北市行政區: ")
+        district += "區"
+        district_stations = []
+        for station in data:
+            if station['sarea'] == district:
+                district_stations.append(station)
+
+
+        if district_stations:
+            pprint(district_stations)
+        else:
+            print(f"沒有找到 {district} 行政區的站點資訊。請再輸入一次")
+            
 
 
 
